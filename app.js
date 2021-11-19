b83a83cf3f6592dccf94d15c19dde7b6a9ec5dee
Preetish Biswal <78357366+Preetishx77@users.noreply.github.com>
2021-11-19 20:29:41 +0530

    Files

---
+++ app.js
@@ -0,0 +1,29 @@
+// const donorlist = document.querySelector('#donor-list');
+// function renderCafe(doc){
+//     let li = document.createElement('li');
+//     let bloodgroup = document.createElement('span');
+//     let name = document.createElement('span');
+//     let number = document.createElement('span');
+//     let pincode = document.createElement('span');
+
+    // li.setAttribute('data-id',doc.id);
+    // bloodgroup.textContent = doc.data().bloodgroup;
+    // name.textContent = doc.data().name;
+    // number.textContent = doc.data().number;
+    // pincode.textContent = doc.data().pincode;
+
+    // li.appendChild(bloodgroup);
+    // li.appendChild(name);
+    // li.appendChild(number);
+    // li.appendChild(pincode);
+
+    // donorlist.appendChild(li);
+// }
+
+
+// db.collection('donors').where('bloodgroup','==','O-').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         // renderCafe(doc);
+//     })
+    
+// })
